968e1aac4925277f739fd00fe184c888825e4b0a
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-10-10 22:22:33 +0100

    Updates

---
+++ precache-manifest.fcf9c282de2ed152f99028ffe53257f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2280f1e7bf413f0cebbb7378262682",
+    "url": "/Cool-Markup/index.html"
+  },
+  {
+    "revision": "c92adce065775fcfa7f3",
+    "url": "/Cool-Markup/static/css/main.5fb2a80b.chunk.css"
+  },
+  {
+    "revision": "7dd729d54cce6a20517d",
+    "url": "/Cool-Markup/static/js/2.50879f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cool-Markup/static/js/2.50879f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92adce065775fcfa7f3",
+    "url": "/Cool-Markup/static/js/main.59f1871b.chunk.js"
+  },
+  {
+    "revision": "6b6cae5c22b7768913ce",
+    "url": "/Cool-Markup/static/js/runtime-main.16842730.js"
+  }
+]);
